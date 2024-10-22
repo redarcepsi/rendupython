32cfa279af567bec2c7cba08c6c15f7a3f2bb8a6
DEPRET Selmac <151008630+redarcepsi@users.noreply.github.com>
2024-10-22 15:01:46 +0200

    rendu final

--- disk.py
+++ disk.py
@@ -3,9 +3,10 @@ class Disk:
         self.total=total
         self.used=used
     
+    @property
     def free(self):
         return self.total-self.used
-    
+    @property
     def used_perc(self):
         return self.used/self.total
     
@@ -13,14 +14,14 @@ class Disk:
         return f"Disk[total : {self.total} Gb, used : {self.used} Gb]"
     
     def __lt__(self,other):
-        return self.used_perc() < other.used_perc()
+        return self.used_perc < other.used_perc
     
 if __name__ == '__main__':
     disk = Disk(10,2)
     disk2 = Disk(20,5)
-    print(disk.free())
-    print(disk2.free())
-    print(disk.used_perc())
-    print(disk2.used_perc())
+    print(disk.free)
+    print(disk2.free)
+    print(disk.used_perc)
+    print(disk2.used_perc)
     disks = [disk, disk2, Disk(2000,1)]
     print(sorted(disks))
